b1c772936a9493c7168e01eeccd473cd537f7ef2
Fco Molina <fmolinaprog@gmail.com>
2017-11-08 22:56:32 +0100

    Arregla evento de posicionamiento.

--- phototracker-app/app/src/main/java/com/fmc/phototracker/MainActivity.java
+++ phototracker-app/app/src/main/java/com/fmc/phototracker/MainActivity.java
@@ -1,13 +1,23 @@
 package com.fmc.phototracker;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,6 +26,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -28,15 +39,19 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, LocationListener {
 
-    private MapView myOpenMapView;
-    private IMapController myMapController;
-    private CompassOverlay mCompassOverlay;
+    MapView myOpenMapView;
+    IMapController myMapController;
+    CompassOverlay mCompassOverlay;
 
-    private Context ctx = getApplicationContext();
+    Location location;
+    double lat, lon, accuracy;
+    long fix;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -85,16 +100,14 @@ public class MainActivity extends AppCompatActivity
         myOpenMapView.setBuiltInZoomControls(true);
 
         myMapController = myOpenMapView.getController();
-        myMapController.setZoom(4);
 
-        GeoPoint center = new GeoPoint(40.416963,-3.703531);
-        myMapController.animateTo(center);
-
-        this.mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx), myOpenMapView);
-        this.mCompassOverlay.enableCompass();
+        Context ctx = getApplicationContext();
+        mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx), myOpenMapView);
+        mCompassOverlay.enableCompass();
         myOpenMapView.getOverlays().add(this.mCompassOverlay);
-    }
 
+        registerLocationListener();
+    }
 
     @Override
     public void onBackPressed() {
@@ -108,34 +121,26 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
         if (id == R.id.routes) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_explore) {
@@ -153,6 +158,111 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    public void registerLocationListener() {
+        LocationManager locationManager;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(false);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_LOW);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        assert locationManager != null;
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        } else {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+        if (location != null) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            fix = location.getTime();
+            accuracy = location.getAccuracy();
+            Toast.makeText(this, "Latitud: " + lat + ", Longitud: " + lon + "\nPrecision: " + accuracy + " m", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No hay localización", Toast.LENGTH_SHORT).show();
+
+        }
+
+        GeoPoint center = new GeoPoint(lat, lon);
+        myMapController.animateTo(center);
+        myMapController.setZoom(10);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                GeocodeTask task = new GeocodeTask();
+                task.execute(new LatLong(lat, lon));
+            }
+        });
+
+        long time = 0;
+        float distance = 0;
+        locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, time, distance, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, time, distance, this);
+        if (locationManager.getAllProviders().contains("network")) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, time, distance, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+    }
+
+    protected class GeocodeTask extends AsyncTask<LatLong, Void, String> {
+        @Override
+        protected String doInBackground(LatLong... params) {
+            Geocoder gc = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresslist = null;
+            StringBuffer returnString = new StringBuffer();
+
+            try {
+                addresslist = gc.getFromLocation(lat, lon, 3);
+
+                if (addresslist == null) {
+                    return "Unknown Address";
+                } else {
+                    if (addresslist.size() > 0) {
+                        Address add = addresslist.get(0);
+                        for (int i = 0; i < add.getMaxAddressLineIndex(); i++) {
+                            returnString.append(add.getAddressLine(i)).append("\n");
+                        }
+
+                        returnString.append(add.getLocality()).append("\n");
+                    }
+                }
+            } catch (IOException e) {
+                returnString = new StringBuffer("Unknown Address");
+            } finally {
+                if (returnString.toString().length() < 5) {
+                    return "Unknown Address";
+                }
+                return returnString.toString();
+            }
+        }
+    }
+
     private void dispatchTakePictureIntent() throws IOException {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -162,18 +272,33 @@ public class MainActivity extends AppCompatActivity
     }
 
     private File createImageFile() throws IOException {
-        // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,
+                ".jpeg",
+                storageDir
         );
 
-        // Save a file: path for use with ACTION_VIEW intents
         String mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    class LatLong {
+        double lat, lon;
+
+        LatLong(double lat, double lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public double getLon() {
+            return lon;
+        }
     }
 }
