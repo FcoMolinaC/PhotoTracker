d9411196f391ca6c134d5c63c6d6cf9797bb0a4b
Fco Molina <fmolinaprog@gmail.com>
2018-01-21 20:08:14 +0100

    Ejecuta las tareas de post en hilos

--- phototracker-app/app/src/main/java/com/fmc/phototracker/Database.java
+++ phototracker-app/app/src/main/java/com/fmc/phototracker/Database.java
@@ -1,5 +1,6 @@
 package com.fmc.phototracker;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.apache.http.NameValuePair;
@@ -20,131 +21,142 @@ import java.util.List;
 
 public class Database {
 
+    private HttpClient httpclient;
+    private List<NameValuePair> param_POST;
+    private HttpPost httppost;
+
     private String resultText;
     private int position = 0;
     private List<User> UserList = null;
     private boolean result = false;
 
-    private final static String URL_SERVIDOR = "";
-    // URL del directorio de los scripts php del servidor
+    private final static String URL_SERVIDOR = "192.168.0.12";
     private final static String URL_PHP = "http://" + URL_SERVIDOR + "/phototrack/";
 
     public boolean insert(String username, String password) {
-        HttpClient httpclient;
-        List<NameValuePair> parametros_POST;
-        HttpPost httppost;
         httpclient = new DefaultHttpClient();
         httppost = new HttpPost(URL_PHP + "insert.php");
 
-        parametros_POST = new ArrayList<NameValuePair>(4);
-        parametros_POST.add(new BasicNameValuePair("username", username.trim()));
-        parametros_POST.add(new BasicNameValuePair("password", password.trim()));
+        param_POST = new ArrayList<NameValuePair>(4);
+        param_POST.add(new BasicNameValuePair("username", username.trim()));
+        param_POST.add(new BasicNameValuePair("password", password.trim()));
 
-        try {
-            httppost.setEntity(new UrlEncodedFormEntity(parametros_POST));
-            httpclient.execute(httppost);
-            result = true;
-        } catch (UnsupportedEncodingException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (IOException e) {
-            result = false;
-            e.printStackTrace();
-        }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    httppost.setEntity(new UrlEncodedFormEntity(param_POST));
+                    httpclient.execute(httppost);
+                    result = true;
+                } catch (UnsupportedEncodingException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    result = false;
+                    e.printStackTrace();
+                }
+            }
+        });
         return result;
     }
 
     public boolean delete(String username) {
-        HttpClient httpclient;
-        List<NameValuePair> parametros_POST;
-        HttpPost httppost;
         httpclient = new DefaultHttpClient();
         httppost = new HttpPost(URL_PHP + "delete.php");
 
-        parametros_POST = new ArrayList<NameValuePair>();
+        param_POST = new ArrayList<NameValuePair>();
 
         User user = UserList.get(position);
         String id = String.valueOf(user.getId());
-        parametros_POST.add(new BasicNameValuePair("id", id));
+        param_POST.add(new BasicNameValuePair("id", id));
 
-        try {
-            httppost.setEntity(new UrlEncodedFormEntity(parametros_POST));
-            httpclient.execute(httppost);
-            result = true;
-        } catch (UnsupportedEncodingException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (IOException e) {
-            result = false;
-            e.printStackTrace();
-        }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    httppost.setEntity(new UrlEncodedFormEntity(param_POST));
+                    httpclient.execute(httppost);
+                    result = true;
+                } catch (UnsupportedEncodingException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    result = false;
+                    e.printStackTrace();
+                }
+            }
+        });
         return result;
     }
 
     public boolean update(String username, String password) {
-        HttpClient httpclient;
-        List<NameValuePair> parametros_POST;
-        HttpPost httppost;
         httpclient = new DefaultHttpClient();
         httppost = new HttpPost(URL_PHP + "update.php");
 
-        parametros_POST = new ArrayList<NameValuePair>(5);
-        parametros_POST.add(new BasicNameValuePair("username", username.trim()));
-        parametros_POST.add(new BasicNameValuePair("password", password.trim()));
+        param_POST = new ArrayList<NameValuePair>(5);
+        param_POST.add(new BasicNameValuePair("username", username.trim()));
+        param_POST.add(new BasicNameValuePair("password", password.trim()));
 
         User user = UserList.get(position);
         String id = String.valueOf(user.getId());
-        parametros_POST.add(new BasicNameValuePair("id", id));
+        param_POST.add(new BasicNameValuePair("id", id));
 
-        try {
-            httppost.setEntity(new UrlEncodedFormEntity(parametros_POST));
-            httpclient.execute(httppost);
-            result = true;
-        } catch (UnsupportedEncodingException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (IOException e) {
-            result = false;
-            e.printStackTrace();
-        }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    httppost.setEntity(new UrlEncodedFormEntity(param_POST));
+                    httpclient.execute(httppost);
+                    result = true;
+                } catch (UnsupportedEncodingException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    result = false;
+                    e.printStackTrace();
+                }
+            }
+        });
         return result;
 
     }
 
     public boolean login(String username, String password) {
-        HttpClient httpclient;
-        List<NameValuePair> parametros_POST;
-        HttpPost httppost;
         httpclient = new DefaultHttpClient();
         httppost = new HttpPost(URL_PHP + "login.php");
 
-        parametros_POST = new ArrayList<NameValuePair>(4);
-        parametros_POST.add(new BasicNameValuePair("username", username.trim()));
-        parametros_POST.add(new BasicNameValuePair("password", password.trim()));
+        param_POST = new ArrayList<NameValuePair>(4);
+        param_POST.add(new BasicNameValuePair("username", username.trim()));
+        param_POST.add(new BasicNameValuePair("password", password.trim()));
 
-        try {
-            httppost.setEntity(new UrlEncodedFormEntity(parametros_POST));
-            httpclient.execute(httppost);
-            result = true;
-        } catch (UnsupportedEncodingException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            result = false;
-            e.printStackTrace();
-        } catch (IOException e) {
-            result = false;
-            e.printStackTrace();
-        }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    httppost.setEntity(new UrlEncodedFormEntity(param_POST));
+                    httpclient.execute(httppost);
+                    result = true;
+                } catch (UnsupportedEncodingException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    result = false;
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    result = false;
+                    e.printStackTrace();
+                }
+            }
+        });
         return result;
     }
 
